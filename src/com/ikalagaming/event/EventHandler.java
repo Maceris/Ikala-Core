25637f2f7ac83e96aaa4453256af12329533850b
KingOfL33t <KingOfL33tGaming@gmail.com>
2015-04-28 16:05:07 -0500

    Rebuilt javadoc

--- src/com/ikalagaming/event/EventHandler.java
+++ src/com/ikalagaming/event/EventHandler.java
@@ -24,6 +24,8 @@ public @interface EventHandler {
 	 * <li>HIGHEST
 	 * <li>MONITOR
 	 * </ol>
+	 * 
+	 * @return Returns this handler's priority
 	 */
 	EventPriority priority() default EventPriority.NORMAL;
 }
