c203c03374d70720472dc2a4fccbfe5e031bc72c
maceris <chesburks@gmail.com>
2021-08-01 23:08:10 -0400

    Add constants class

---
+++ src/com/ikalagaming/launcher/Constants.java
@@ -0,0 +1,53 @@
+package com.ikalagaming.launcher;
+
+import java.io.File;
+
+/**
+ * Constants that are used for the framework.
+ *
+ * @author Ches Burks
+ *
+ */
+public class Constants {
+
+	/**
+	 * The path to the configuration folder for each plugin. This should be set
+	 * up with path separators so that you can do something like
+	 * {@code "ExamplePlugin" + CONFIG_PATH + "example.yml"} and get a valid
+	 * path to a file.
+	 */
+	public static final String CONFIG_PATH =
+		File.separator + "config" + File.separator;
+
+	/**
+	 * The path to the data folder for each plugin. This should be set up with
+	 * path separators so that you can do something like
+	 * {@code "ExamplePlugin" + DATA_PATH + "example.yml"} and get a valid path
+	 * to a file.
+	 */
+	public static final String DATA_PATH =
+		File.separator + "data" + File.separator;
+
+	/**
+	 * The path to the plugins from the current folder. This should be set up
+	 * with path separators so that you can do something like
+	 * {@code System.getProperty("user.dir") + PLUGIN_FOLDER_PATH + "Example.jar"}
+	 * and get a valid path to a plugin.
+	 */
+	public static final String PLUGIN_FOLDER_PATH =
+		File.separator + "plugins" + File.separator;
+
+	/**
+	 * The path to the scripts folder for each plugin. This should be set up
+	 * with path separators so that you can do something like
+	 * {@code "ExamplePlugin" + SCRIPTS_PATH + "example.yml"} and get a valid
+	 * path to a file.
+	 */
+	public static final String SCRIPTS_PATH =
+		File.separator + "scripts" + File.separator;
+
+	/**
+	 * Private constructor so this class is not initialized.
+	 */
+	private Constants() {}
+}
