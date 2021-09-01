6a2d6e3d1ee638263d111fffac20c5255d8b451b
maceris <chesburks@gmail.com>
2021-08-31 21:03:26 -0400

    Add more event listener tests

---
+++ test/com/ikalagaming/event/CustomEvent.java
@@ -0,0 +1,16 @@
+package com.ikalagaming.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * An event created for unit tests.
+ * 
+ * @author Ches Burks
+ *
+ */
+@AllArgsConstructor
+@Getter
+public class CustomEvent extends Event {
+	private final String testString;
+}
--- test/com/ikalagaming/event/TestEventManager.java
+++ test/com/ikalagaming/event/TestEventManager.java
@@ -1,9 +1,12 @@
 package com.ikalagaming.event;
 
+import org.awaitility.Awaitility;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Tests for the {@link EventManager} class.
@@ -18,7 +21,7 @@ public class TestEventManager {
 	 * @author Ches Burks
 	 *
 	 */
-	public class TestEvent extends Event {}
+	public static class TestEvent extends Event {}
 
 	/**
 	 * Tear down after the tests, destroying the event manager.
@@ -47,6 +50,63 @@ public class TestEventManager {
 		EventManager.getInstance().fireEvent(e);
 		Assert.assertTrue("Class was not fired",
 			EventAssert.wasFired(TestEvent.class, 1000));
+	}
+
+	/**
+	 * Tests the order that event listeners get called in, and that all get
+	 * called.
+	 */
+	@Test
+	public void testEventOrder() {
+		EventManager manager = EventManager.getInstance();
+		EventOrderListener listener = new EventOrderListener();
+
+		manager.registerEventListeners(listener);
+
+		EventAssert.listenFor(EventOrderListener.SimpleEvent.class);
+
+		new EventOrderListener.SimpleEvent().fire();
+
+		Awaitility.await().atMost(1000, TimeUnit.MILLISECONDS).until(
+			() -> EventAssert.wasFired(EventOrderListener.SimpleEvent.class));
+		EventAssert.stopListeningForEverything();
+		Assert.assertTrue(listener.isEverythingCalled());
+	}
+
+	/**
+	 * Register a normal event listener, see if it handles events, then
+	 * unregister and make sure it stops getting events.
+	 */
+	@Test
+	public void testNormalListenerFlow() {
+		EventManager manager = EventManager.getInstance();
+		CustomEventListener listener = new CustomEventListener();
+
+		final String message = "Test123 message.";
+
+		manager.registerEventListeners(listener);
+		CustomEvent event = new CustomEvent(message);
+		manager.fireEvent(event);
+
+		Awaitility.await().atMost(1000, TimeUnit.MILLISECONDS)
+			.until(listener::isEventReceived);
+
+		Assert.assertTrue(listener.isEventReceived());
+		Assert.assertEquals(message, listener.getRecordedData());
+
+		listener.clear();
+
+		manager.unregisterEventListeners(listener);
+
+		EventAssert.listenFor(CustomEvent.class);
+
+		manager.fireEvent(event);
+
+		Awaitility.await().atMost(1000, TimeUnit.MILLISECONDS)
+			.until(() -> EventAssert.wasFired(CustomEvent.class));
+
+		Assert.assertFalse(listener.isEventReceived());
+		EventAssert.stopListeningForEverything();
 	}
 
 }
