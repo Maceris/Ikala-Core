10609b0279ef079db08230954b2bfbe39ef0d6de
David Burks <chesburks@gmail.com>
2023-01-24 19:29:40 -0500

    Add cancelable event

---
+++ src/main/java/com/ikalagaming/event/CancelableEvent.java
@@ -0,0 +1,19 @@
+package com.ikalagaming.event;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * An abstract event that can be canceled.
+ */
+@Getter
+@Setter
+public abstract class CancelableEvent extends Event {
+	/**
+	 * Whether the event has been canceled. If it has, it will not be processed.
+	 * 
+	 * @param canceled Whether we want to cancel the event.
+	 * @return Whether the event is canceled.
+	 */
+	private boolean canceled = false;
+}
