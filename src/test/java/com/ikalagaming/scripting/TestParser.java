3c8860bfc60350b0779ca4c85c4531d792446dd0
David Burks <chesburks@gmail.com>
2023-04-01 21:04:14 -0400

    Add smoke test for the parser

---
+++ src/test/java/com/ikalagaming/scripting/TestParser.java
@@ -0,0 +1,63 @@
+package com.ikalagaming.scripting;
+
+import com.ikalagaming.scripting.IkalaScriptParser.CompilationUnitContext;
+import com.ikalagaming.scripting.ast.AbstractSyntaxTree;
+import com.ikalagaming.scripting.ast.CompilationUnit;
+
+import org.antlr.v4.runtime.BufferedTokenStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.TokenStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Tests the functionality of the scripting engine class.
+ *
+ * @author Ches Burks
+ *
+ */
+public class TestParser {
+
+	/**
+	 * The resources folder where the test scripts are located.
+	 */
+	private final String TEST_SCRIPT_FOLDER =
+		System.getProperty("user.dir") + File.separatorChar + "build"
+			+ File.separatorChar + "resources" + File.separatorChar + "test"
+			+ File.separatorChar + "com" + File.separatorChar + "ikalagaming"
+			+ File.separatorChar + "scripting" + File.separatorChar;
+
+	/**
+	 * Tries to evaluate some lua code that prints text.
+	 */
+	@Test
+	public void testPrinting() {
+		CharStream stream;
+		try {
+			stream = CharStreams
+				.fromFileName(this.TEST_SCRIPT_FOLDER + "ParserSmoke.iks");
+		}
+		catch (IOException e) {
+			Assertions.fail(e);
+			return;
+		}
+
+		IkalaScriptLexer lexer = new IkalaScriptLexer(stream);
+		TokenStream tokenStream = new BufferedTokenStream(lexer);
+		IkalaScriptParser parser = new IkalaScriptParser(tokenStream);
+
+		CompilationUnitContext context = parser.compilationUnit();
+
+		Assertions.assertEquals(0, parser.getNumberOfSyntaxErrors(),
+			"Parser should have no syntax errors");
+
+		CompilationUnit ast = AbstractSyntaxTree.process(context);
+		ast.processTreeTypes();
+		Assertions.assertTrue(ast.validateTree(), "Validation should pass");
+	}
+
+}
