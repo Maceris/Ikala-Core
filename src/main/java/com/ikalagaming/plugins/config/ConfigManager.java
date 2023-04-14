090643a0f61551efacd2f79612606538a814e7db
David Burks <chesburks@gmail.com>
2023-04-14 19:10:30 -0400

    Update and document config class

--- src/main/java/com/ikalagaming/plugins/config/ConfigManager.java
+++ src/main/java/com/ikalagaming/plugins/config/ConfigManager.java
@@ -3,6 +3,7 @@ package com.ikalagaming.plugins.config;
 import com.ikalagaming.launcher.PluginFolder;
 import com.ikalagaming.launcher.PluginFolder.ResourceType;
 import com.ikalagaming.plugins.Plugin;
+import com.ikalagaming.plugins.PluginManager;
 
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -14,11 +15,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Handles reading, writing, and caching configurations.
- * 
+ *
  * @author Ches Burks
  *
  */
@@ -30,19 +33,100 @@ public class ConfigManager {
 	 */
 	public static final String DEFAULT_NAME = "config.yml";
 
-	private PluginConfig emptyConfig() {
-		return null;
+	private static Map<String, PluginConfig> configCache = new HashMap<>();
+
+	/**
+	 * Create an empty configuration to use if none is present.
+	 *
+	 * @param configName The name of the configuration, since a plugin might
+	 *            have multiple.
+	 * @return The configuration.
+	 */
+	private static PluginConfig emptyConfig(@NonNull String configName) {
+		return new PluginConfig(new HashMap<>());
 	}
 
-	private static PluginConfig getDefaultConfig(@NonNull String pluginName) {
+	private static String getCacheName(@NonNull String pluginName,
+		@NonNull String configName) {
+		// $ is not a valid character in plugin names
+		return String.format("%s$%s", pluginName, configName);
+	}
 
-		return null;
+	/**
+	 * Load the default configuration for a plugin. If no configuration exists
+	 * on disk, a blank one will be returned.
+	 *
+	 * @param pluginName The plugin to load configuration for.
+	 * @return The default configuration for the given plugin.
+	 */
+	public static PluginConfig loadConfig(@NonNull String pluginName) {
+		return ConfigManager.loadConfig(pluginName, ConfigManager.DEFAULT_NAME);
+	}
+
+	/**
+	 * Load an arbitrary configuration for a plugin. If no configuration exists
+	 * on disk, a blank one will be returned.
+	 *
+	 * @param pluginName The plugin to load configuration for.
+	 * @param configName The configuration file to load. Should be of the format
+	 *            like "example.yml".
+	 * @return The associated configuration.
+	 */
+	public static PluginConfig loadConfig(@NonNull String pluginName,
+		@NonNull String configName) {
+
+		final String cacheName =
+			ConfigManager.getCacheName(pluginName, configName);
+		if (ConfigManager.configCache.containsKey(cacheName)) {
+			return ConfigManager.configCache.get(cacheName);
+		}
+		File configFile = PluginFolder.getResource(pluginName,
+			ResourceType.CONFIG, configName);
+		if (!configFile.exists()
+			&& PluginManager.getInstance().isLoaded(pluginName)) {
+
+			// Try and copy in the case we are still enabling the plugin
+			Optional<Plugin> maybePlugin =
+				PluginManager.getInstance().getPlugin(pluginName);
+			if (maybePlugin.isPresent()
+				&& !PluginManager.getInstance().isEnabled(pluginName)) {
+				ConfigManager.log.debug(
+					"We are still loading {}, but a missing config was requested, attempting to create the missing {} config",
+					pluginName, configName);
+				ConfigManager.saveDefaultConfig(maybePlugin.get(), configName);
+			}
+		}
+		if (!configFile.exists()) {
+			ConfigManager.log.debug(
+				"Can't find config {} for the {} plugin, using a blank default",
+				configName, pluginName);
+			PluginConfig cached = ConfigManager.emptyConfig(configName);
+			ConfigManager.configCache.put(cacheName, cached);
+			return cached;
+		}
+
+		try {
+			InputStream stream = new FileInputStream(configFile);
+			Yaml yaml = new Yaml();
+			Map<String, Object> contents = yaml.load(stream);
+
+			PluginConfig cached = new PluginConfig(contents);
+			ConfigManager.configCache.put(cacheName, cached);
+			return cached;
+		}
+		catch (FileNotFoundException e) {
+			ConfigManager.log
+				.warn("File does not exist after verifying it exists", e);
+		}
+		PluginConfig cached = ConfigManager.emptyConfig(configName);
+		ConfigManager.configCache.put(cacheName, cached);
+		return cached;
 	}
 
 	/**
 	 * Save a copy of the default configuration file if it is missing. This will
 	 * not not overwrite an existing configuration.
-	 * 
+	 *
 	 * @param owner The plugin that owns the configuration.
 	 * @param configName The name of the configuration file.
 	 */
@@ -52,7 +136,8 @@ public class ConfigManager {
 		try (InputStream in =
 			owner.getClass().getClassLoader().getResourceAsStream(configName)) {
 			if (in == null) {
-				log.debug("Can't find config {} in the jar for the {} plugin",
+				ConfigManager.log.debug(
+					"Can't find config {} in the jar for the {} plugin",
 					configName, owner.getName());
 				return;
 			}
@@ -60,8 +145,9 @@ public class ConfigManager {
 			File target = PluginFolder.getResource(owner.getName(),
 				ResourceType.CONFIG, configName);
 			if (target.exists()) {
-				log.debug("Config {} already exists for the {} plugin",
-					configName, owner.getName());
+				ConfigManager.log.debug(
+					"Config {} already exists for the {} plugin", configName,
+					owner.getName());
 				return;
 			}
 
@@ -69,34 +155,9 @@ public class ConfigManager {
 		}
 		catch (IOException ignored) {
 			// No default config exists
-			log.debug("Exception when adding config {} for the {} plugin",
-				configName, owner.getName());
+			ConfigManager.log.debug(
+				"Exception when adding config {} for the {} plugin", configName,
+				owner.getName());
 		}
-	}
-
-	public static PluginConfig getConfig(@NonNull String pluginName) {
-		return getConfig(pluginName, DEFAULT_NAME);
-	}
-
-	public static PluginConfig getConfig(@NonNull String pluginName,
-		@NonNull String configName) {
-
-		File configFile = PluginFolder.getResource(pluginName,
-			ResourceType.CONFIG, configName);
-		if (!configFile.exists()) {
-			return getDefaultConfig(pluginName);
-		}
-
-		try {
-			InputStream stream = new FileInputStream(configFile);
-			Yaml yaml = new Yaml();
-			Map<String, Object> contents = yaml.load(stream);
-
-		}
-		catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
--- src/main/java/com/ikalagaming/plugins/config/PluginConfig.java
+++ src/main/java/com/ikalagaming/plugins/config/PluginConfig.java
@@ -1,24 +1,320 @@
 package com.ikalagaming.plugins.config;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
 
 /**
- * Plugin configuration.
- * 
+ * Plugin configuration. This class is a wrapper around a nested map structure
+ * that allows us to more easily interface with it and convert keys to
+ * appropriate types.
+ *
  * @author Ches Burks
  *
  */
+@Slf4j
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class PluginConfig {
 
+	/**
+	 * The actual contents of the configuration, a nested map structure.
+	 */
+	private final Map<String, Object> contents;
+
+	/**
+	 * Handle the casting to an appropriate type, logging a warning if it fails.
+	 *
+	 * @param <T> The type to cast to, assumed from context.
+	 * @param object The object to cast.
+	 * @return The object, cast to the appropriate type, or null if there was an
+	 *         exception.
+	 */
+	@SuppressWarnings("unchecked")
+	private <T> T cast(@NonNull Object object) {
+		try {
+			return (T) object;
+		}
+		catch (ClassCastException e) {
+			PluginConfig.log.warn("Invalid type {}", e.getLocalizedMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Access a generic type from the config. If the key cannot be found, or the
+	 * key is not of the generic type, null is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param <T> The resulting type.
+	 * @param key The path to the key.
+	 * @return The key, or null if the key is missing.
+	 */
+	public <T> T get(@NonNull String key) {
+		return this.navigateKeys(key, null);
+	}
+
+	/**
+	 * Access a boolean from the config. If the key cannot be found, false is
+	 * returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or false if the key is missing.
+	 */
 	public boolean getBoolean(@NonNull String key) {
-		return false;
+		return this.navigateKeys(key, Boolean.FALSE);
 	}
 
+	/**
+	 * Access a list of booleans from the config. If the key cannot be found, an
+	 * empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<Boolean> getBooleanList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
+	}
+
+	/**
+	 * Access a list of bytes from the config. If the key cannot be found, an
+	 * empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<Byte> getByteList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
+	}
+
+	/**
+	 * Access a list of characters from the config. If the key cannot be found,
+	 * an empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<Character> getCharacterList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
+	}
+
+	/**
+	 * Access a double from the config. If the key cannot be found, 0 is
+	 * returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or 0 if the key is missing.
+	 */
+	public double getDouble(@NonNull String key) {
+		return this.navigateKeys(key, 0d);
+	}
+
+	/**
+	 * Access a list of doubles from the config. If the key cannot be found, an
+	 * empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<Double> getDoubleList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
+	}
+
+	/**
+	 * Access an integer from the config. If the key cannot be found, 0 is
+	 * returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or 0 if the key is missing.
+	 */
 	public int getInt(@NonNull String key) {
-		return 0;
+		return this.navigateKeys(key, 0);
 	}
 
-	public String getString(@NonNull String key) {
-		return "";
+	/**
+	 * Access a list of integers from the config. If the key cannot be found, an
+	 * empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<Integer> getIntList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
 	}
+
+	/**
+	 * Access a generic list from the config. If the key cannot be found, an
+	 * empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<?> getList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
+	}
+
+	/**
+	 * Access a long from the config. If the key cannot be found, 0 is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or 0 if the key is missing.
+	 */
+	public long getLong(@NonNull String key) {
+		return this.navigateKeys(key, 0L);
+	}
+
+	/**
+	 * Access a list of longs from the config. If the key cannot be found, an
+	 * empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<Long> getLongList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
+	}
+
+	/**
+	 * Access a generic type from the config. If the key cannot be found, or the
+	 * key is not of the generic type, the default value is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param <T> The resulting type.
+	 * @param key The path to the key.
+	 * @param defaultValue The value to return if the key is not found. May be
+	 *            null.
+	 * @return The key, or the default value if the key is missing.
+	 */
+	public <T> T getOrDefault(@NonNull String key, T defaultValue) {
+		return this.navigateKeys(key, defaultValue);
+	}
+
+	/**
+	 * Access a list of shorts from the config. If the key cannot be found, an
+	 * empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<Short> getShortList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
+	}
+
+	/**
+	 * Access a string from the config. If the key cannot be found, an empty
+	 * string is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty string if the key is missing.
+	 */
+	public String getString(@NonNull String key) {
+		return this.navigateKeys(key, "");
+	}
+
+	/**
+	 * Access a list of strings from the config. If the key cannot be found, an
+	 * empty unmodifiable list is returned.
+	 *
+	 * Keys are accessed by name, and nested keys are accessed by the full path
+	 * to the key with dot separators. For example, {@code "simple-key"} and
+	 * {@code "more.complex.nested-key"} are valid keys.
+	 *
+	 * @param key The path to the key.
+	 * @return The key, or an empty list if the key is missing.
+	 */
+	public List<String> getStringList(@NonNull String key) {
+		return this.navigateKeys(key, List.of());
+	}
+
+	/**
+	 * Dig through the config to find the specified value.
+	 *
+	 * @param <T> The type to return.
+	 * @param keyPath The key, as a path like "test", or
+	 *            "test.other.longer-name".
+	 * @param defaultValue The default value to return if an entry is not found.
+	 * @return The resulting value, or a default one if it is not found.
+	 */
+	private <T> T navigateKeys(@NonNull String keyPath, T defaultValue) {
+		String[] parts = keyPath.split("\\.");
+		Map<String, Object> currentMap = this.contents;
+		for (int i = 0; i < parts.length; ++i) {
+			if (i == parts.length - 1) {
+				return this
+					.cast(currentMap.getOrDefault(parts[i], defaultValue));
+			}
+			if (currentMap.get(parts[i]) == null) {
+				StringBuilder invalidName = new StringBuilder();
+				for (int j = 0; j <= i; ++j) {
+					invalidName.append(parts[j]);
+				}
+				PluginConfig.log.warn("Invalid key {}", invalidName.toString());
+				return defaultValue;
+			}
+			currentMap = this.cast(currentMap.get(parts[i]));
+		}
+		return defaultValue;
+	}
+
 }
