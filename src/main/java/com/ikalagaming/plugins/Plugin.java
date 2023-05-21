c5b15a15c425d6d1d1ce7af22610a5fcd44e2b78
David Burks <chesburks@gmail.com>
2023-05-21 10:26:19 -0400

    Remove abstract constructor

--- src/main/java/com/ikalagaming/plugins/Plugin.java
+++ src/main/java/com/ikalagaming/plugins/Plugin.java
@@ -25,13 +25,6 @@ public abstract class Plugin {
 	private Set<Listener> emptyListenerSet = new HashSet<>();
 
 	/**
-	 * You should not be creating plugins using the constructor.
-	 *
-	 * @see PluginManager#getPlugin(String)
-	 */
-	public Plugin() {}
-
-	/**
 	 * Returns a list of listeners for this plugin. These listeners will be used
 	 * with the event system.
 	 *
