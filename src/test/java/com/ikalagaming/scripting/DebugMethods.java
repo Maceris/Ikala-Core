45edd2720439363db6825d2ba7756c6670ce7874
David Burks <chesburks@gmail.com>
2023-05-24 21:46:51 -0400

    Add tests for static and instance method calls with multiple parameters

--- src/test/java/com/ikalagaming/scripting/DebugMethods.java
+++ src/test/java/com/ikalagaming/scripting/DebugMethods.java
@@ -1,6 +1,7 @@
 package com.ikalagaming.scripting;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +15,122 @@ import java.util.List;
 public class DebugMethods {
 
 	/**
+	 * Used for testing method calls, object typing.
+	 *
+	 * @author Ches Burks
+	 *
+	 */
+	@Getter
+	@Setter
+	public static class TestObject {
+		/**
+		 * An integer value.
+		 */
+		int integer;
+		/**
+		 * A double value.
+		 */
+		double doub;
+		/**
+		 * A string value.
+		 */
+		String string;
+	}
+
+	/**
 	 * The output from the program.
 	 */
 	@Getter
 	private static List<String> output = new ArrayList<>();
+	/**
+	 * Checks if we have successfully validated the values in
+	 * {@link #TEST_checkValues(String, int, double, TestObject)}.
+	 */
+	@Getter
+	private static boolean checkValuesMatched = false;
 
 	/**
 	 * Reset for the next test. Intended to be used by the test suite.
 	 */
 	public static void reset() {
 		DebugMethods.output.clear();
+		DebugMethods.checkValuesMatched = false;
+	}
+
+	/**
+	 * Checks that the values in the parameters equal to the same ones returned
+	 * by the getters in this class.
+	 *
+	 * @param string The string that should match the value given by
+	 *            {@link #TEST_getString()}.
+	 * @param integer The integer that should match the value given by
+	 *            {@link #TEST_getInt()}.
+	 * @param doub The string that should match the value given by
+	 *            {@link #TEST_getDouble()}.
+	 * @param o The object that should exist and contain values that match the
+	 *            aforementioned ones.
+	 */
+	public static void TEST_checkValues(String string, int integer, double doub,
+		TestObject o) {
+
+		if (o == null) {
+			System.out.printf("%s %d %.4f, (null)\n", string, integer, doub);
+		}
+		else {
+			System.out.printf("%s %d %.4f, (%s %d %.4f)\n", string, integer,
+				doub, o.getString(), o.getInteger(), o.getDoub());
+		}
+		if (string == null || !string.equals(DebugMethods.TEST_getString())
+			|| (integer != DebugMethods.TEST_getInt())
+			|| (doub != DebugMethods.TEST_getDouble())) {
+			DebugMethods.checkValuesMatched = false;
+			return;
+		}
+		if ((o == null) || o.getString() == null
+			|| !o.getString().equals(DebugMethods.TEST_getString())
+			|| (o.getInteger() != DebugMethods.TEST_getInt())
+			|| (o.getDoub() != DebugMethods.TEST_getDouble())) {
+			DebugMethods.checkValuesMatched = false;
+			return;
+		}
+		DebugMethods.checkValuesMatched = true;
+
+	}
+
+	/**
+	 * Fetches a static double.
+	 *
+	 * @return The double.
+	 */
+	public static double TEST_getDouble() {
+		return 120.9214;
+	}
+
+	/**
+	 * Fetches a static integer.
+	 *
+	 * @return The integer.
+	 */
+	public static int TEST_getInt() {
+		return -4567;
+	}
+
+	/**
+	 * Returns a test object.
+	 *
+	 * @return The new object.
+	 */
+	public static TestObject TEST_getObject() {
+		return new TestObject();
+	}
+
+	/**
+	 * Fetches a static string.
+	 *
+	 * @return The string.
+	 */
+	public static String TEST_getString() {
+		return "Sample string!";
 	}
 
 	/**
--- src/test/java/com/ikalagaming/scripting/TestBehavior.java
+++ src/test/java/com/ikalagaming/scripting/TestBehavior.java
@@ -90,4 +90,44 @@ class TestBehavior {
 		DebugMethods.reset();
 	}
 
+	/**
+	 * Tests some method calls.
+	 */
+	@Test
+	void testMethodCalls() {
+		final String program = """
+			double d;
+			String s;
+			int i = TEST_getInt();
+			s = TEST_getString();
+			d = TEST_getDouble();
+			TestObject o = TEST_getObject();
+			o.setInteger(i);
+			o.setString(s);
+			o.setDoub(d);
+			TEST_checkValues(s, i, d, o);
+			""";
+
+		CharStream stream = CharStreams.fromString(program);
+		Optional<ScriptRuntime> maybeRuntime =
+			IkalaScriptCompiler.parse(stream);
+
+		ScriptManager.registerClass(DebugMethods.class);
+
+		Assertions.assertTrue(maybeRuntime.isPresent());
+		ScriptRuntime runtime = maybeRuntime.get();
+
+		// Sanity check so builds finish
+		final int MAX_INSTRUCTIONS = 10_000;
+		int instructions = 0;
+		while (!runtime.hasTerminated() && instructions < MAX_INSTRUCTIONS) {
+			runtime.step();
+			++instructions;
+		}
+
+		Assertions.assertTrue(DebugMethods.isCheckValuesMatched());
+
+		DebugMethods.reset();
+	}
+
 }
